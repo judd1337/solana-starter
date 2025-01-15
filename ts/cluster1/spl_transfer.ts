a7a8ca406bb91429660b077dddbf8fadf7e5097d
David Lindstrom <david.lindstroem@gmail.com>
2025-01-15 21:40:04 +0100

    coding lecture 2

--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,6 +1,8 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import wallet from "../turbin3-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
+import { connect } from "http2";
+import { keypairIdentity } from "@metaplex-foundation/umi";
 
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,7 +12,7 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("5kWTyiAZ27u4j16sv8rjChNKS61WdhjRtHBFHifAinS2");
 
 // Recipient address
 const to = new PublicKey("<receiver address>");
@@ -18,11 +20,16 @@ const to = new PublicKey("<receiver address>");
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey, false, commitment);
+        console.log(`Your ata is: ${fromAta.address.toBase58()}`);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
+        const toAta = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to, false, commitment);
+        console.log(`Your ata is: ${toAta.address.toBase58()}`);
 
         // Transfer the new token to the "toTokenAccount" we just created
+        const tx = await transfer(connection, keypair, fromAta.address, toAta.address, keypair, 2e6);
     } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
+        console.error(`Oops, something went wrong: ${e}`);
     }
 })();
