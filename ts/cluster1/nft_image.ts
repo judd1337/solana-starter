087f5cf8327304441bf2e43ec51f4cea9f99c43b
David Lindstrom <david.lindstroem@gmail.com>
2025-01-16 19:32:36 +0100

    lecture 3 - upload/metadata/mint nft

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../turbin3-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -6,6 +6,7 @@ import { readFile } from "fs/promises"
 
 // Create a devnet connection
 const umi = createUmi('https://api.devnet.solana.com');
+const image_path = "./generug.png"; 
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -16,13 +17,16 @@ umi.use(signerIdentity(signer));
 (async () => {
     try {
         //1. Load image
+        const image = await readFile(image_path);
+
         //2. Convert image to generic file.
+        const genericFile = createGenericFile(image, "generug.png", {displayName: "Rug", contentType: "image/png",});
+
         //3. Upload image
-
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
+        const [myUri] = await umi.uploader.upload([genericFile]);
+        //await irysUploader();
+        
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
